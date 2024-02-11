0b164c189c69b48a846db85df3b919a29098fb9d
Sidney François <sidney.fvm.dev@outlook.com>
2024-02-11 18:10:18 -0300

    feat: add middleware.

--- src/modules/cat/cats.module.ts
+++ src/modules/cat/cats.module.ts
@@ -1,10 +1,28 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { CatsController } from './cats.controller';
 import { CatsService } from './cats.service';
+import { LoggerMiddleware } from '../middlewares/logger.middleware';
+import { logger } from '../middlewares/logger.function.middleware';
 
 @Module({
   controllers: [CatsController],
   providers: [CatsService],
   exports: [CatsService],
 })
-export class CatsModule {}
+
+// Different middleware for GET and POST
+export class CatsModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware)
+      .forRoutes({ path: 'cats', method: RequestMethod.GET });
+    consumer
+      .apply(logger)
+      .forRoutes({ path: 'cats', method: RequestMethod.POST });
+  }
+}
